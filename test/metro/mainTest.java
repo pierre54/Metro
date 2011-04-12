410ec3539c5c46559c685d4f4b92765c012b98be
Aurélie Salaün <Aurelie@MacAurelie.local>
2011-04-12 20:39:20 +0200

    erreur test unitaire LaPlusProche

--- test/metro/mainTest.java
+++ test/metro/mainTest.java
@@ -3,26 +3,43 @@ package metro;
 
 import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class mainTest {
-		
-	private Station[] stationsAttendues = new Station[10];
-	private Coordonnee coorUnA = new Coordonnee (0,0);
-	private Station UnA = new Station("UnA",40,coorUnA);
-	private Coordonnee coorUnB = new Coordonnee (10,0);
-	private Station UnB = new Station("UnB",40,coorUnB);
-	private Coordonnee coorUnC = new Coordonnee (20,0);
-	private Station UnC = new Station("UnC",40,coorUnC);
-	private Coordonnee coorUnD = new Coordonnee (30,0);
-	private Station UnD = new Station("UnD",40,coorUnD);
 	
-	Station[0].add(UnA);
-	Station[1].add(UnB);
-	Station[2].add(UnC);
-	Station[3].add(UnD);
+	private Station[] stations= new Station[10];
+	private Station[] stationsAttendues= new Station[10];
+	private Coordonnee coorUnA = new Coordonnee(0,0);
+	private Station unA = new Station("UnA",40,coorUnA);
+	private Coordonnee coorUnB = new Coordonnee(60,0);
+	private Station unB = new Station("UnB",40,coorUnB);
+	private Coordonnee coorUnC = new Coordonnee(110,0);
+	private Station unC = new Station("UnC",40,coorUnC);
+	private Coordonnee coorUnD = new Coordonnee(200,0);
+	private Station unD = new Station("UnD",40,coorUnD);
+	private Coordonnee homme = new Coordonnee(80,0);
 	
 	@Before
-	public void setUp() throws Exception {	
+	public void setUp() throws Exception {
+		stationsAttendues[0]= unB;
+		stationsAttendues[1]= unC;
+		
+		stations[0]= unA;
+		stations[1]= unB;
+		stations[2]= unC;
+		stations[3]= unD;
+		
 	}
-
+	
+	@Test
+	public void testlaPlusProche(){
+		System.out.println("stationLaPlusProche");
+		stationsAttendues[0]= unB;
+		stationsAttendues[1]= unC;
+		
+		Station[] a = main.laPlusProche(stations, homme);
+		assertArrayEquals(stationsAttendues, a);
+	}
+	
+	
 }
